6d3bc19ed76d648c00d3b4a703043570dabc1b82
Yamisagi <erencandan20@gmail.com>
2023-11-16 22:43:09 +0300

    feat: Add new dependencies and update navbar component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
+  return <div>
+    
+  </div>;
+};
 
-export default Home
+export default Home;
